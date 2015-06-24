77ba565fee3c44e7832fcb9bf185a68de3d18fce
nekocode <syfyw@qq.com>
2015-06-24 23:10:18 +0800

    use ICECSS framwork

--- onecampus_tools.py
+++ onecampus_tools.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 __author__ = 'nekocode'
 
+import os
 import tornado.ioloop
 import tornado.web
 import httplib2
 import json
-
 
 class OpenDoorHandler(tornado.web.RequestHandler):
     def data_received(self, chunk):
@@ -23,7 +23,7 @@ class OpenDoorHandler(tornado.web.RequestHandler):
 
     __login_body = "tel=13246823364&password=110110zxc"
 
-    __opendoor_body = 'communityId=385&doorIdStr=0551'
+    __opendoor_body = 'communityId=385&doorIdStr=02'
 
     def login(self):
         url = 'http://www.uhomecp.com/userInfo/login.json'
@@ -54,23 +54,29 @@ class OpenDoorHandler(tornado.web.RequestHandler):
             _rlt = False
         return _rlt
 
-    def get(self):
-        self.write('<html><body><form action="/" method="post">'
-                   '<input type="submit" value="打开后门~">'
-                   '</form></body></html>')
-
     def post(self):
         token = self.login()
         if token:
             if self.opendoor(token):
-                self.write('开门成功 o(≧口≦)o ')
+                self.write('suc')
             else:
-                self.write('好像出了点问题 ╮(╯▽╰)╭\n你可以尝试联系开发者 wechat: nekocode')
+                self.write('failed')
 
+class MainHandler(tornado.web.RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+    def get(self):
+        self.render('index.html')
+
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), "static")
+}
 
 application = tornado.web.Application([
-    (r"/tools/open_door", OpenDoorHandler)
-])
+    (r"/", MainHandler),
+    (r"/tools/opendoor", OpenDoorHandler)
+], **settings)
 
 if __name__ == "__main__":
     application.listen(8888)
